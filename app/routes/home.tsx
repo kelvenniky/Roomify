a4191c870ca91d0a5c3edfffbbc8dc44acd28c71
Kelvinafutu <kelvin@emit-labs.com>
2026-07-03 05:54:33 +0000

    Implement home Screen

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,7 @@
+import { ArrowRight, Clock, Layers } from "lucide-react";
 import Header from "../../components/Header";
 import type { Route } from "./+types/home";
+import Button from "../../components/ui/Button";
 
 
 
@@ -15,7 +17,70 @@ export default function Home() {
   return (
     <div className="home">
       <Header/>
-      <h1>hello</h1>
+      <section className="hero">
+          <div className="announce">
+            <div className="dot">
+              <div className="pulse"/>
+            </div>
+            <p>Introducing ROoMIFY 2.0</p>
+          </div>
+          <h1>Build beautiful spaces at the speed of thought with Roomify</h1>
+          <p className="subtitle">Roomify is an AI first design envirronment that helps you visualize. render and ship arcihitectural projects faster than ever.</p>
+          <div className="actions">
+            <a href="#upload" className="cta">
+              Start building <ArrowRight className="icon"/>
+            </a>
+            <Button variant="outline" size="lg" className="demo">
+              Watch Demo
+            </Button>
+          </div>
+          <div id="upload" className="upload-shell">
+            <div className="grid-overlay"/>
+            <div className="upload-card">
+              <div className="upload-head">
+                <div className="upload-icon">
+                  <Layers className="icon"/>
+                </div>
+                <h3>Upload your floor plan</h3>
+                <p>Supports JPG, PNG , formats up to 10MB</p>
+              </div>
+              <p>Upload images</p>
+            </div>
+          </div>
+      </section>
+      <section className="projects">
+          <div className="section-inner">
+            <div className="section-head">
+              <div className="copy">
+                  <h2>Projects</h2>
+                  <p>Your latest work and shared community projects all in one place.</p>
+              </div>
+            </div>
+            <div className="projects-grid">
+              <div className="project-card group">
+                <div className="preview">
+                  <img src="https://roomify-mlhuk267-dfwu1i.puter.site/projects/1770803585402/rendered.png" alt="Floor plan" />
+                  <div className="badge">
+                    <span>Community</span>
+                  </div>
+                </div>
+                <div className="card-body">
+                  <div>
+                    <h3>Project Manhattan</h3>
+                    <div className="meta">
+                      <Clock size={12}/>
+                      <span>{new Date('01-10-2026').toLocaleDateString()}</span>
+                      <span>By Kelven</span>
+                    </div>
+                  </div>
+                  <div className="arrow">
+                    <ArrowRight size={18}/>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+      </section>
 
     </div>
   )
