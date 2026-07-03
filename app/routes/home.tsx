7ae8c5f90fa5092bd7dd55a365c9a1ef338928a1
Kelvinafutu <kelvin@emit-labs.com>
2026-07-03 05:13:16 +0000

    first commit

---
+++ app/routes/home.tsx
@@ -0,0 +1,22 @@
+import Header from "../../components/Header";
+import type { Route } from "./+types/home";
+
+
+
+
+export function meta({}: Route.MetaArgs) {
+  return [
+    { title: "New React Router App" },
+    { name: "description", content: "Welcome to React Router!" },
+  ];
+}
+
+export default function Home() {
+  return (
+    <div className="home">
+      <Header/>
+      <h1>hello</h1>
+
+    </div>
+  )
+}
